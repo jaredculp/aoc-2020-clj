58823a2e30a8548506a1d9e0055018a587591e3b
jaredculp <jculp14@gmail.com>
2020-12-03 10:31:56 -0500

    day3, part1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -12,6 +12,11 @@
   [in]
   (map read-string in))
 
+(defn is-tree?
+  "Whether or not the current position 'x' on a line is a tree (marked by '#')"
+  [x line]
+  (= \# (nth (cycle line) x)))
+
 (defn day1
   "--- Day 1: Report Repair ---
   After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
@@ -109,6 +114,66 @@
                    (= (nth pass second-pos) letter))))
        count))
 
+(defn day3
+  "--- Day 3: Toboggan Trajectory ---
+  With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+  Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+  ..##.......
+  #...#...#..
+  .#....#..#.
+  ..#.#...#.#
+  .#...##..#.
+  ..#.##.....
+  .#.#.#....#
+  .#........#
+  #.##...#...
+  #...##....#
+  .#..#...#.#
+  These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+  ..##.........##.........##.........##.........##.........##.......  --->
+  #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+  .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+  ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+  .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+  ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+  .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+  .#........#.#........#.#........#.#........#.#........#.#........#
+  #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+  #...##....##...##....##...##....##...##....##...##....##...##....#
+  .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+  You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+  The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+  From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+  The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+  ..##.........##.........##.........##.........##.........##.......  --->
+  #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+  .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+  ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+  .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+  ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+  .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+  .#........#.#........X.#........#.#........#.#........#.#........#
+  #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+  #...##....##...##....##...#X....##...##....##...##....##...##....#
+  .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+  In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+  Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+  "
+
+  []
+  (->> (input "resources/day3.txt")
+       (drop 1)
+       (keep-indexed #(when (is-tree? (* (inc %1) 3) %2) true))
+       count))
+
 (defn -main
   []
   ; answer: 646779
@@ -121,4 +186,7 @@
   (println (str "day2-part1: " (day2-1)))
 
   ; answer: 584
-  (println (str "day2-part2: " (day2-2))))
+  (println (str "day2-part2: " (day2-2)))
+
+  ; answer: 181
+  (println (str "day3-part1: " (day3))))
