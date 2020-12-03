f26e1615f23db7180e47010657549d2d39daeb59
jaredculp <jculp14@gmail.com>
2020-12-03 09:03:42 -0500

    add cider-nrepl for autocomplete

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[cljfmt "0.7.0"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.6"]]
-  :plugins [[lein-cljfmt "0.7.0"]]
+  :plugins [[lein-cljfmt "0.7.0"]
+            [cider/cider-nrepl "0.25.3"]]
   :main advent.core)
